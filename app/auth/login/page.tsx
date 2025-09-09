2466c7e8e83d2a08b0b327ae64f02eb3df940c12
cesarr340 <cesarr340@gmail.com>
2025-09-09 23:51:09 +0000

    inicio da página de login

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,17 @@
+import './style.css'
+
+export function Login(){
+    return(
+        <main id="login">
+            <div>
+                imagem
+            </div>
+
+            <div>
+                formulário
+            </div>
+        </main>
+    )
+}
+
+export default Login
