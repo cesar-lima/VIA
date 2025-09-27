7b9d9465e33dd321d09b03d68db25413c878af77
cesar-lima <cesarr340@gmail.com>
2025-09-26 23:41:26 -0300

    acrescentar mais um icone no card para acessar a aba de comentários do restaurante

--- app/components/Card/Card.tsx
+++ app/components/Card/Card.tsx
@@ -1,44 +1,48 @@
 import Link from 'next/link'
 import './card.css'
 
-import { Copy, Utensils } from 'lucide-react'
+import { Copy, MessageCircleMore, Utensils } from 'lucide-react'
 import { MapPinned } from 'lucide-react'
 
 export default function Card() {
     return (
-        <Link href="../../restaurant">
-            <div className="card-container">
-                <div className="card-header">
-                    <div className="header-top">
-                        <Utensils color="#212842" />
+        <div className="card-container">
+            <div className="card-header">
+                <div className="header-top">
+                    <Utensils color="#212842" />
+                </div>
+                <div className="restaurant-name">Sukiya - Liberdade</div>
+                <div className="handle">
+                    <div>
+                        Av. liberdade, 123, Liberdade
                     </div>
-                    <div className="restaurant-name">Sukiya - Liberdade</div>
-                    <div className="handle">
-                        <div>
-                            Av. liberdade, 123, Liberdade
-                        </div>
+
+                    <div className="handle-buttons">
+                        <Link href="../../restaurant">
+                            <MessageCircleMore />
+                        </Link>
 
                         <div>
-                            <Copy color="#212842" />
+                            <Copy />
                         </div>
 
-                        <Link href="">
-                            <MapPinned color="#212842" />
+                        <Link href="https://maps.google.com" target="_blank" rel="noopener noreferrer">
+                            <MapPinned />
                         </Link>
                     </div>
                 </div>
+            </div>
 
-                <div className="card-stats">
-                    <div className="stat">
-                        <div className="stat-number">755</div>
-                        <div className="stat-label">Avaliações</div>
-                    </div>
-                    <div className="stat">
-                        <div className="stat-number">3/5.0</div>
-                        <div className="stat-label">Nota</div>
-                    </div>
+            <div className="card-stats">
+                <div className="stat">
+                    <div className="stat-number">755</div>
+                    <div className="stat-label">Avaliações</div>
+                </div>
+                <div className="stat">
+                    <div className="stat-number">3/5.0</div>
+                    <div className="stat-label">Nota</div>
                 </div>
             </div>
-        </Link>
+        </div>
     )
 }
