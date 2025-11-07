79ed964a98d68585cfebd714c6ddb9ab494a5337
cesar-lima <cesarr340@gmail.com>
2025-11-06 23:37:25 -0300

    criação da página para visualização dos dados do usuário e alteração

--- app/pages/profile/page.tsx
+++ app/pages/profile/page.tsx
@@ -1,12 +1,78 @@
+"use client"
+
 import Navbar from '../../components/Navbar/Navbar'
 import './style.css'
 
 export default function Profile() {
-    return(
-        <div id="profile">
-            <Navbar /> 
+    const handleSave = (e: React.FormEvent) => {
+        e.preventDefault()
+        console.log('Perfil salvo:')
+    }
 
-            
+    return (
+        <div id="profile">
+            <Navbar />
+
+            <h2>Dados da Conta</h2>
+
+            <form className="profile-form" onSubmit={handleSave}>
+                {/* nome */}
+                <div className="profile-input">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon lucide lucide-user-round-icon lucide-user-round">
+                        <circle cx="12" cy="8" r="5" />
+                        <path d="M20 21a8 8 0 0 0-16 0" />
+                    </svg>
+                    <input
+                        type="text"
+                        name="nome"
+                        className="input"
+                        placeholder="Nome"
+                        required
+                    />
+                </div>
+
+                {/* apelido */}
+                <div className="profile-input">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon lucide lucide-tag-icon lucide-tag">
+                        <path d="M12.586 2.586A2 2 0 0 0 11.172 2H4a2 2 0 0 0-2 2v7.172a2 2 0 0 0 .586 1.414l8.704 8.704a2.426 2.426 0 0 0 3.42 0l6.58-6.58a2.426 2.426 0 0 0 0-3.42z" />
+                        <circle cx="7.5" cy="7.5" r=".5" fill="currentColor" />
+                    </svg>
+                    <input
+                        type="text"
+                        name="apelido"
+                        className="input"
+                        placeholder="Apelido"
+                        required
+                    />
+                </div>
+
+                {/* cep */}
+                <div className="profile-input">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon lucide lucide-map-pin-house-icon lucide-map-pin-house">
+                        <path d="M15 22a1 1 0 0 1-1-1v-4a1 1 0 0 1 .445-.832l3-2a1 1 0 0 1 1.11 0l3 2A1 1 0 0 1 22 17v4a1 1 0 0 1-1 1z" />
+                        <path d="M18 10a8 8 0 0 0-16 0c0 4.993 5.539 10.193 7.399 11.799a1 1 0 0 0 .601.2" />
+                        <path d="M18 22v-3" />
+                        <circle cx="10" cy="10" r="3" />
+                    </svg>
+                    <input
+                        type="text"
+                        name="cep"
+                        className="input"
+                        placeholder="CEP"
+                        required
+                    />
+                </div>
+
+                <button className="register-button" type="submit">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="sparkle lucide lucide-sparkles-icon lucide-sparkles">
+                        <path d="M11.017 2.814a1 1 0 0 1 1.966 0l1.051 5.558a2 2 0 0 0 1.594 1.594l5.558 1.051a1 1 0 0 1 0 1.966l-5.558 1.051a2 2 0 0 0-1.594 1.594l-1.051 5.558a1 1 0 0 1-1.966 0l-1.051-5.558a2 2 0 0 0-1.594-1.594l-5.558-1.051a1 1 0 0 1 0-1.966l5.558-1.051a2 2 0 0 0 1.594-1.594z" />
+                        <path d="M20 2v4" />
+                        <path d="M22 4h-4" />
+                        <circle cx="4" cy="20" r="2" />
+                    </svg>
+                    <span className="text">Salvar</span>
+                </button>
+            </form>
         </div>
     )
 }
